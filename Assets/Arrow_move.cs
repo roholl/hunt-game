a83442addae36b229b03d505a72925df41e66054
Robert Hollenstain <robert.d.hollenstain@gmail.com>
2018-12-19 12:11:07 -0600

    Reverted to Sprite Collision detection and ability to walk up slopes

--- Assets/Arrow_move.cs
+++ Assets/Arrow_move.cs
@@ -10,10 +10,13 @@ public class Arrow_move : MonoBehaviour {
     public float gravY;
     public float bulletLifeTime;
 
+    private bool hitObject;
+
 
 	// Use this for initialization
 	void Start () {
 
+        this.hitObject = false;
 
 	}
 	
@@ -21,28 +24,43 @@ public class Arrow_move : MonoBehaviour {
 	void Update () {
 
 
-        var oldx = transform.position.x;
-        var oldy = transform.position.y;
-
-        this.ySpeed = this.ySpeed + (this.gravY * Time.deltaTime);
-        
-        var yMove = this.ySpeed * Time.deltaTime;
-        var xMove = this.xSpeed * Time.deltaTime;
-        var directionNew = (new Vector3(transform.position.x + xMove, transform.position.y + yMove) - transform.position);
-        
-        transform.Translate(new Vector2(xMove, yMove ), Space.World);
-        var distanceX = transform.position.x - oldx;
-        var distanceY = transform.position.y - oldy;
-
-        var newRadians = Mathf.Atan2(distanceY, distanceX);
-        transform.rotation = Quaternion.Euler(0, 0, (newRadians * Mathf.Rad2Deg));
+       
 
 
+    }
+
+    private void FixedUpdate()
+    {
+        if(!this.hitObject)//if not stopped
+        {
+            var oldx = transform.position.x;
+            var oldy = transform.position.y;
+
+            this.ySpeed = this.ySpeed + (this.gravY * Time.deltaTime);
+
+            var yMove = this.ySpeed * Time.deltaTime;
+            var xMove = this.xSpeed * Time.deltaTime;
+            var directionNew = (new Vector3(transform.position.x + xMove, transform.position.y + yMove) - transform.position);
+
+            transform.Translate(new Vector2(xMove, yMove), Space.World);
+            var distanceX = transform.position.x - oldx;
+            var distanceY = transform.position.y - oldy;
+
+            var newRadians = Mathf.Atan2(distanceY, distanceX);
+            transform.rotation = Quaternion.Euler(0, 0, (newRadians * Mathf.Rad2Deg));
+        }
     }
 
     private void Awake()
     {
         Destroy(this.gameObject, this.bulletLifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.hitObject = true;
+
+        print("Derp hit");
     }
 }
 
--- Assets/Hunter/Scripts/Player/PlayerController.cs
+++ Assets/Hunter/Scripts/Player/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public int currHealth;
     public int maxHealth;
     public float playerGravity;
+    public float maxClimbAngle;
+
 
 
     float ySpeed = 0;
@@ -21,20 +23,23 @@ public class PlayerController : MonoBehaviour
     bool isWalking = false;
     bool fixBottom = false;
 
+
+
     private Animator animator;
     private SpriteRenderer spRender;
     private BoxCollider2D bbox;
     private float prevY;
-    
+    private Vector3 velocity;
+
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-      
+
         spRender = animator.GetComponent<SpriteRenderer>();
         // GetComponent<SpriteRenderer>();
         bbox = GetComponent<BoxCollider2D>();
-        
+
     }
 
     void Update()
@@ -47,12 +52,12 @@ public class PlayerController : MonoBehaviour
         //var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         this.isWalking = true;
-        if(x > 0)
+        if (x > 0)
         {
             xScale = 1;
             this.isFacingRight = true;
         }
-        else if(x < 0)
+        else if (x < 0)
         {
             xScale = -1;
             this.isFacingRight = false;
@@ -62,79 +67,125 @@ public class PlayerController : MonoBehaviour
             this.isWalking = false;
         }
 
-       
 
 
-      //  var bbox = GetComponent<BoxCollider2D>(); //Players Collider
+
+        //  var bbox = GetComponent<BoxCollider2D>(); //Players Collider
         var bound1 = bbox.bounds.min;//Min x and y points on collider
         var bound2 = bbox.bounds.max;//Max x and y point on collider 
 
         var y = 0f;
 
-        if(isBlockedX(bound1, bound2, Mathf.Abs(x)))
-        {
-            x = 0;
-        }
+      
 
-        if(!isGrounded(bound1))
+        if (!isGrounded(bound1, bound2, Mathf.Abs(this.ySpeed)))
         {
             this.ySpeed = this.ySpeed + this.playerGravity;
-            if(!this.hasDoubleJumped && jumpDown)
+            if (!this.hasDoubleJumped && jumpDown)
             {
                 this.ySpeed = jumpPower;
                 this.hasDoubleJumped = true;
             }
         }
-        else if(ySpeed <= 0)
+        else if (ySpeed <= 0)
         {
             this.hasJumped = this.hasDoubleJumped = false;
-            if(jumpDown)
+            if (jumpDown)
             {
                 this.hasJumped = true;
                 this.ySpeed = this.jumpPower;
             }
             else
             {
-                
-                RaycastHit2D hit = Physics2D.Raycast(bound1, Vector2.down,.1f, 1 << 8);
-                if(hit.distance >.01f)
+
+                RaycastHit2D hit;
+               // RaycastHit2D hit = Physics2D.Raycast(bound1, Vector2.down, .1f, 1 << 8);
+
+                RaycastHit2D hit1 = Physics2D.Raycast(bound1, Vector2.down, .1f, 1 << 8);
+                RaycastHit2D hit2 = Physics2D.Raycast(bound2, Vector2.down, .1f, 1 << 8);
+
+
+                float hitDistance = isGroundedF(bound1, bound2, Mathf.Abs(this.ySpeed));
+
+                if(hit1.distance <= hit2.distance)
                 {
-                    this.ySpeed = -(hit.distance -.01f);
-                    fixBottom = true;
+                    hit = hit1;
+                }
+                else
+                {
+                    hit = hit2;
+                }
+
+               // Debug.DrawRay(bound1, Vector2.down, Color.yellow);
+                if (hit.distance > .01f)
+                {
+                    // this.ySpeed = -(hit.distance - .01f);
+                    this.ySpeed = hitDistance;
+                    //fixBottom = true;
                 }
                 else
                 {
                     this.ySpeed = 0;
                 }
-                
-            } 
+
+            }
         }
-        if(ySpeed > 0)//check if his dumb head will hit something while jumping
+        if (ySpeed > 0)//check if his dumb head will hit something while jumping
         {
-            if(isBlockedY(bound1, bound2))
+            if (isBlockedY(bound1, bound2))
             {
                 this.ySpeed = 0;
             }
         }
 
         y = ySpeed * Time.deltaTime;
-        if(fixBottom && (ySpeed < 0))
+       /* if (fixBottom && (ySpeed < 0))
         {
             y = ySpeed;
             fixBottom = false;
-        } 
+        }
+        */
+
+        if (isBlockedX(bound1, bound2, (Mathf.Abs(x) > .05f) ? Mathf.Abs(x): .05f))
+        {
+            RaycastHit2D hitBottom = isBlockedBottomRightX(bound1, bound2, Mathf.Abs(x));
+
+            float slopeAngle = Vector2.Angle(hitBottom.normal, Vector2.up);
+
+
+
+            if (slopeAngle <= maxClimbAngle)
+            {
+                velocity.x = x;
+                velocity.y = y;
+                ClimbSlope(ref velocity, slopeAngle);
+                x = velocity.x;
+                y = velocity.y;
+            }
+            else
+            {
+                x = 0;
+            }
+            
+        }
+
+
         transform.Translate(x, y, 0);
         //ySpeed = y;
-      //  animator.SetFloat("move", move);
+        //  animator.SetFloat("move", move);
 
         animator.SetBool("isWalking", this.isWalking);
         animator.SetBool("isFacingRight", this.isFacingRight);
-        if(this.ySpeed < 0)
+      /*  if (this.ySpeed < 0)
         {
             print(this.ySpeed);
         }
+        */
         animator.SetBool("isFalling", (this.ySpeed < -.01) ? true : false);
-   
+
+
+        
+
     }
 
     private void LateUpdate()
@@ -150,15 +201,69 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool isGrounded(Vector3 v3)
+    private bool isGrounded(Vector3 min, Vector3 max, float yDist)
     {
+        Vector2 leftPoint;
+        Vector2 rightPoint;
+
+
+        leftPoint = new Vector2(min.x, min.y);
+        rightPoint = new Vector2(max.x, min.y);
+
         var origin = transform.position;
-        origin.y = v3.y;
+        origin.y = min.y;
         int layerMask = 1 << 8;
         Debug.DrawRay(origin, -Vector3.up, Color.red);
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down);
-        return Physics2D.Raycast(origin, Vector2.down, .1f, layerMask);
+        Debug.DrawRay(leftPoint, Vector2.down, Color.red);
+        Debug.DrawRay(rightPoint, Vector2.down, Color.red);
+        //Debug
+        bool hit = false;
+
+     //   hit  = Physics2D.Raycast(origin, Vector2.down);
+        hit |= Physics2D.Raycast(leftPoint, Vector2.down, .1f, layerMask);
+        hit |=  Physics2D.Raycast(rightPoint, Vector2.down, .1f, layerMask);
+        //return Physics2D.Raycast(origin, Vector2.down, .1f, layerMask);
+        return (hit);
     }
+
+    private float isGroundedF(Vector3 min, Vector3 max, float yDist)
+    {
+        Vector2 leftPoint;
+        Vector2 rightPoint;
+
+
+        leftPoint = new Vector2(min.x, min.y);
+        rightPoint = new Vector2(max.x, min.y);
+
+        var origin = transform.position;
+        origin.y = min.y;
+        int layerMask = 1 << 8;
+        Debug.DrawRay(origin, -Vector3.up, Color.red);
+        Debug.DrawRay(leftPoint, Vector2.down, Color.red);
+        Debug.DrawRay(rightPoint, Vector2.down, Color.red);
+        //Debug
+        float hit = -1.0f;
+
+        //   hit  = Physics2D.Raycast(origin, Vector2.down);
+        RaycastHit2D hit1 = Physics2D.Raycast(leftPoint, Vector2.down, .1f, layerMask);
+        RaycastHit2D hit2 = Physics2D.Raycast(rightPoint, Vector2.down, .1f, layerMask);
+
+        if(hit1.distance <= hit2.distance)
+        {
+            hit = hit1.distance;
+        }
+        else
+        {
+            hit = hit2.distance;
+        }
+
+        //hit |= Physics2D.Raycast(leftPoint, Vector2.down, .1f, layerMask);
+        //hit |= Physics2D.Raycast(rightPoint, Vector2.down, .1f, layerMask);
+        //return Physics2D.Raycast(origin, Vector2.down, .1f, layerMask);
+        return (hit);
+    }
+
+    // private 
 
     private bool isBlockedX(Vector3 min, Vector3 max, float xDist)
     {
@@ -166,7 +271,7 @@ public class PlayerController : MonoBehaviour
         Vector2 bottomPoint;
 
         Vector2 direction;
-        if(isFacingRight)
+        if (isFacingRight)
         {
             direction = Vector2.right;
             //max x, max y
@@ -185,14 +290,51 @@ public class PlayerController : MonoBehaviour
         }
 
         int layerMask = 1 << 8;
-     //   layerMask = ~layerMask;
+        //   layerMask = ~layerMask;
         Debug.DrawRay(topPoint, direction, Color.blue);
         Debug.DrawRay(bottomPoint, direction, Color.blue);
 
         bool hit = false;
 
-        hit = Physics2D.Raycast(topPoint, direction,  xDist, layerMask);
+        hit = Physics2D.Raycast(topPoint, direction, xDist, layerMask);
         hit |= Physics2D.Raycast(bottomPoint, direction, xDist, layerMask);
+        return (hit);
+
+    }
+    
+
+    private RaycastHit2D isBlockedBottomRightX(Vector3 min, Vector3 max, float xDist)
+    {
+        Vector2 topPoint;
+        Vector2 bottomPoint;
+
+        Vector2 direction;
+        if (isFacingRight)
+        {
+            direction = Vector2.right;
+            //max x, max y
+            //max x, min y
+         //   topPoint = new Vector2(max.x, max.y);
+            bottomPoint = new Vector2(max.x, min.y);
+
+        }
+        else
+        {
+            //min x, max y
+            //min x, min y
+            //topPoint = new Vector2(min.x, max.y);
+            bottomPoint = new Vector2(min.x, min.y);
+            direction = Vector2.left;
+        }
+
+        int layerMask = 1 << 8;
+        //   layerMask = ~layerMask;
+    //    Debug.DrawRay(topPoint, direction, Color.blue);
+      //  Debug.DrawRay(bottomPoint, direction, Color.blue);
+
+        //bool hit = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(bottomPoint, direction, xDist, layerMask);
         return (hit);
 
     }
@@ -203,7 +345,27 @@ public class PlayerController : MonoBehaviour
         Vector2 rightPoint = new Vector2(max.x, max.y);
 
 
+       // bool hit = false;
+
+       // hit = Physics2D.Raycast(topPoint, direction, xDist, layerMask);
+        //hit |= Physics2D.Raycast(bottomPoint, direction, xDist, layerMask);
+
         return false;
 
     }
+
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDistance = Mathf.Abs(velocity.x);
+
+        velocity.y = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Hello");
+    }
+
+
 }
